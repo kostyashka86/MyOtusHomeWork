674450b943ba0c2a17c4aee41bb8642ea96c4f73
k.shishmagaev <k.shishmagaev@qiwi.com>
2023-01-12 14:40:05 +0300

    added jenkins groovy script and allure in pom

---
+++ jenkins/ui-autotests.groovy
@@ -0,0 +1,34 @@
+timeout(60) {
+    node('maven-slave') {
+        timestamps {
+            wrap([$class: 'BuildUser']) {
+                ownerInfo = """<b>Owner:</b> ${env.BUILD_USER}"""
+                currentBuild.description = ownerInfo
+            }
+            stage('Checkout') {
+                checkout scm
+            }
+            stage('Running UI tests') {
+
+                def exitCode = sh(
+                        returnStatus: true,
+                        script: """
+                mvn clean test -Dbrowser="chrome" -Dfilter="QA"
+                """
+                )
+                if (exitCode == 1) {
+                    currentBuild.result = 'UNSTABLE'
+                }
+            }
+            stage('reports') {
+                allure([
+                        includeProperties: false,
+                        jdk              : '',
+                        properties       : [],
+                        reportBuildPolicy: 'ALWAYS',
+                        results          : [[path: 'allure-results']]
+                ])
+            }
+        }
+    }
+}
