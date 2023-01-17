eea23a52416f5e105eebe089e7c32fc288502887
k.shishmagaev <k.shishmagaev@qiwi.com>
2023-01-17 16:03:19 +0300

    pipe

--- jenkins/ui-autotests.groovy
+++ jenkins/ui-autotests.groovy
@@ -1,34 +1,28 @@
 timeout(60) {
     node('maven-slave') {
-        timestamps {
-            wrap([$class: 'BuildUser']) {
-                ownerInfo = """<b>Owner:</b> ${env.BUILD_USER}"""
-                currentBuild.description = ownerInfo
-            }
-            stage('Checkout') {
-                checkout scm
-            }
-            stage('Running UI tests') {
+        stage('Checkout') {
+            checkout scm
+        }
+        stage('Running UI tests') {
 
-                def exitCode = sh(
-                        returnStatus: true,
-                        script: """
+            def exitCode = sh(
+                    returnStatus: true,
+                    script: """
                 mvn clean test -Dbrowser=$BROWSER -Dfilter=$FILTER
                 """
-                )
-                if (exitCode == 1) {
-                    currentBuild.result = 'UNSTABLE'
-                }
+            )
+            if (exitCode == 1) {
+                currentBuild.result = 'UNSTABLE'
             }
-            stage('reports') {
-                allure([
-                        includeProperties: false,
-                        jdk              : '',
-                        properties       : [],
-                        reportBuildPolicy: 'ALWAYS',
-                        results          : [[path: 'allure-results']]
-                ])
-            }
+        }
+        stage('reports') {
+            allure([
+                    includeProperties: false,
+                    jdk              : '',
+                    properties       : [],
+                    reportBuildPolicy: 'ALWAYS',
+                    results          : [[path: 'allure-results']]
+            ])
         }
     }
 }
